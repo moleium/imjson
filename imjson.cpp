600bb66b515d4901830620ee172e1f909a6e6388
Mole <93382765+MoleSchizo@users.noreply.github.com>
2023-09-08 13:31:31 +0300

    Error handling enhancements

--- imjson.cpp
+++ imjson.cpp
@@ -1,14 +1,3 @@
-//===-- imjson/imjson.cpp - imjson library implementation ------*- C++ -*-===//
-//
-// Part of the imjson project
-//
-//===----------------------------------------------------------------------===//
-//
-// This file contains the implementation of the imjson library, which allows
-// theme definition for imgui through JSON files.
-//
-//===----------------------------------------------------------------------===//
-
 #include "imjson.h"
 
 #include <fstream>
@@ -16,10 +5,8 @@
 
 namespace imjson {
 
-	// helper functions to set imgui style colors & variables from a JSON object
-	
 	void set_style_colors(const json& j) {
-		IMJSON_TRY
+		try {
 			for (auto it = j.begin(); it != j.end(); ++it) {
 				const std::string& name = it.key();
 				const json& color = it.value();
@@ -31,11 +18,14 @@ namespace imjson {
 					}
 				}
 			}
-		IMJSON_CATCH("Error setting style colors")
+		}
+		catch (const std::exception& e) {
+			std::cerr << "Error setting style colors: " << e.what() << std::endl;
+		}
 	}
 
 	void set_style_vars(const json& j) {
-		IMJSON_TRY
+		try {
 			for (auto it = j.begin(); it != j.end(); ++it) {
 				const std::string& name = it.key();
 				const json& value = it.value();
@@ -44,12 +34,14 @@ namespace imjson {
 					ImGui::PushStyleVar(style_it->second, value);
 				}
 			}
-		IMJSON_CATCH("Error setting style variables")
+		}
+		catch (const std::exception& e) {
+			std::cerr << "Error setting style variables: " << e.what() << std::endl;
+		}
 	}
 
-	// helper function to load fonts
 	void load_fonts(const json& j) {
-		IMJSON_TRY
+		try {
 			if (j.contains("Default") && j["Default"].is_object()) {
 				const json& default_font = j["Default"];
 				if (default_font.contains("Path") && default_font["Path"].is_string() &&
@@ -60,29 +52,49 @@ namespace imjson {
 					io.Fonts->AddFontFromFileTTF(path.c_str(), size_pixels);
 				}
 			}
-		IMJSON_CATCH("Error loading fonts")
+		}
+		catch (const std::exception& e) {
+			std::cerr << "Error loading fonts: " << e.what() << std::endl;
+		}
 	}
 
 	void load_theme_from_file(const std::string& filename) {
-		IMJSON_TRY
+		try {
 			std::ifstream file(filename);
-		if (file.is_open()) {
-			json j;
-			file >> j;
-			load_theme_from_json(j);
+			if (file.is_open()) {
+				json j;
+				file >> j;
+				load_theme_from_json(j);
+			}
 		}
-		IMJSON_CATCH("Error loading theme from file")
+		catch (const nlohmann::detail::parse_error& e) {
+			std::cerr << "Error loading theme from file: " << e.what() << '\n';
+			std::cerr << "Exception id: " << e.id << '\n';
+			std::cerr << "Byte position of error: " << e.byte << '\n';
+		}
+		catch (const std::exception& e) {
+			std::cerr << "Error loading theme from file: " << e.what() << '\n';
+		}
 	}
 
 	void load_theme_from_string(const std::string& json_str) {
-		IMJSON_TRY
+		try {
 			json j = json::parse(json_str);
-		load_theme_from_json(j);
-		IMJSON_CATCH("Error loading theme from string")
+			load_theme_from_json(j);
+		}
+		catch (const nlohmann::detail::parse_error& e) {
+			// output exception information
+			std::cerr << "Error loading theme from string: " << e.what() << '\n';
+			std::cerr << "Exception id: " << e.id << '\n';
+			std::cerr << "Byte position of error: " << e.byte << '\n';
+		}
+		catch (const std::exception& e) {
+			std::cerr << "Error loading theme from string: " << e.what() << '\n';
+		}
 	}
 
 	void load_theme_from_json(const json& j) {
-		IMJSON_TRY
+		try {
 			for (auto it = j.begin(); it != j.end(); ++it) {
 				const std::string& name = it.key();
 				const json& value = it.value();
@@ -99,6 +111,9 @@ namespace imjson {
 					ImGui::PushStyleVar(style_map.at(name), ImVec2(value[0], value[1]));
 				}
 			}
-		IMJSON_CATCH("Error loading theme from JSON")
+		}
+		catch (const std::exception& e) {
+			std::cerr << "Error loading theme from json: " << e.what() << std::endl;
+		}
 	}
 }
